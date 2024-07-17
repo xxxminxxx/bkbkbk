31dadd7ef14dabe78937f93577aec285dffca579
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-17 12:15:34 +0900

    csv파일 저장기능 구현

--- com.bookbookbook/WebSocketController.java
+++ com.bookbookbook/WebSocketController.java
@@ -3,25 +3,44 @@ package com.bookbookbook;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
-import com.bookbookbook.ChatMessageVO.MessageType;
+import com.bookbookbook.domain.ChatMessageVO;
+import com.bookbookbook.domain.ChatMessageVO.MessageType;
+import com.bookbookbook.service.ChatMessageCSVService;
+import com.bookbookbook.service.ChatRoomService;
 
 @Controller
 public class WebSocketController {
-
+	@Autowired
+    private ChatMessageCSVService chatMessageCSVService;
+	
     private final SimpMessageSendingOperations messagingTemplate;
+	//private final ChatRoomService chatRoomService;
 
     public WebSocketController(SimpMessageSendingOperations messagingTemplate, ChatRoomService chatRoomService) {
         this.messagingTemplate = messagingTemplate;
+        //this.chatRoomService = chatRoomService;
     }
 
+    
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessageVO register(@Payload ChatMessageVO message) {
+        // 클라이언트가 JOIN 메시지를 보냈을 때 처리
+        System.out.println("Received JOIN message: " + message);
+        return message;
+    }
+    
     @MessageMapping("/chat.sendMessage")
     public void sendMessage(@Payload ChatMessageVO chatMessage) {
         chatMessage.setTime(LocalDateTime.now());
+        chatMessageCSVService.saveChatMessage(chatMessage);
         messagingTemplate.convertAndSend("/topic/public", chatMessage);
     }
 
@@ -38,5 +57,5 @@ public class WebSocketController {
         chatMessage.setTime(LocalDateTime.now());
         messagingTemplate.convertAndSend("/topic/public", chatMessage);
     }
-
+    
 }
