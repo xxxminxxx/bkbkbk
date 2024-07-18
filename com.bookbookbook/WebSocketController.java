98f801986d87e2b283c531dc478b6bd5ba8a3a12
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-18 18:02:23 +0900

    Update WebSocketController.java

--- com.bookbookbook/WebSocketController.java
+++ com.bookbookbook/WebSocketController.java
@@ -1,6 +1,8 @@
 package com.bookbookbook;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -19,14 +21,13 @@ public class WebSocketController {
 	@Autowired
     private ChatMessageCSVService chatMessageCSVService;
 	
+	private Set<String> activeUsers = new HashSet<>(); // 유저 목록 저장을 위한 Set
     private final SimpMessageSendingOperations messagingTemplate;
-
 
     public WebSocketController(SimpMessageSendingOperations messagingTemplate) {
         this.messagingTemplate = messagingTemplate;
     }
 
-    
     @MessageMapping("/chat.register")
     @SendTo("/topic/public")
     public ChatMessageVO register(@Payload ChatMessageVO message) {
@@ -44,17 +45,26 @@ public class WebSocketController {
 
     @MessageMapping("/chat.addUser/{roomNum}")
     public void addUser(@Payload ChatMessageVO chatMessage,@DestinationVariable String roomNum) {
-        chatMessage.setType(MessageType.JOIN);
+    	String info = roomNum+"/"+chatMessage.getSender(); // 유저 이름을 가져옴
+        activeUsers.add(info); // 유저를 목록에 추가
+    	chatMessage.setType(MessageType.JOIN);
         chatMessage.setTime(LocalDateTime.now());
-        messagingTemplate.convertAndSend("/topic/public/"+ roomNum, chatMessage);
-        
+        messagingTemplate.convertAndSend("/topic/public/"+ roomNum, chatMessage);     
+        sendActiveUsers(roomNum); // 업데이트된 유저 목록 전송
     }
     
     @MessageMapping("/chat.removeUser/{roomNum}")
     public void removeUser(@Payload ChatMessageVO chatMessage,@DestinationVariable String roomNum) {
-        chatMessage.setType(MessageType.LEAVE);
+    	String info = roomNum+"/"+chatMessage.getSender(); // 유저 이름을 가져옴
+        activeUsers.remove(info); // 유저를 목록에서 제거
+    	chatMessage.setType(MessageType.LEAVE);
         chatMessage.setTime(LocalDateTime.now());
         messagingTemplate.convertAndSend("/topic/public/"+roomNum, chatMessage);
+        sendActiveUsers(roomNum); // 업데이트된 유저 목록 전송
+    }
+    // 유저 목록을 클라이언트에 전송하는 메소드
+    private void sendActiveUsers(String roomNum) {
+        messagingTemplate.convertAndSend("/topic/activeUsers/" + roomNum, activeUsers);
     }
     
 }
