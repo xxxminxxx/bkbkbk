1efca31c3d9c37df9c391c1945e85afb73d6a3e5
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-22 18:39:35 +0900

    채팅방 삭제

--- com.bookbookbook.dao/MainDAO.java
+++ com.bookbookbook.dao/MainDAO.java
@@ -8,8 +8,18 @@ import com.bookbookbook.domain.ChatroomVO;
 
 @Mapper
 public interface MainDAO {
+
+	//##################################
+	//채팅방 불러오기
 	public List<ChatroomVO> getChatroom();
+	//빈 방 삭제
+	//public void emptyRoom();
+	//방 생성
 	public void createChatroom(ChatroomVO chat);
+	//방 검색
 	public List<ChatroomVO> searchChatroom(String keyword);
+	//방 정보 가져오기
 	public ChatroomVO getRoomInfo(String roomNum);
+	//방 삭제
+	public void deleteChatroom(String roomNum);
 }
