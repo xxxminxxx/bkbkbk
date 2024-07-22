f05669733868afa7911cb28aa267348645667e88
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-22 14:37:29 +0900

    카테고리 소분류 탭 추가

--- com.bookbookbook.dao/LibraryDAO.java
+++ com.bookbookbook.dao/LibraryDAO.java
@@ -1,5 +1,6 @@
 package com.bookbookbook.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -11,5 +12,7 @@ import com.bookbookbook.domain.CategoryVO;
 public interface LibraryDAO {
 	//책 카테고리 검색
 	List<CategoryVO> getCategory(CategoryVO cate);
+	List<CategoryVO> getCategorySecond(CategoryVO cate);
 	List<BookVO> getCateBookList(HashMap map);
+	
 }
