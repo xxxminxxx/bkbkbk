786163772438a8ad7fe1a74097a2e06b72f82ca1
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-22 18:39:22 +0900

    채팅방 삭제

--- com.bookbookbook.service/MainServiceImpl.java
+++ com.bookbookbook.service/MainServiceImpl.java
@@ -9,28 +9,34 @@ import com.bookbookbook.dao.MainDAO;
 import com.bookbookbook.domain.ChatroomVO;
 
 @Service
-public class MainServiceImpl implements MainService{
+public class MainServiceImpl implements MainService {
 
 	@Autowired
-	MainDAO mainDAO;
+	private MainDAO mainDAO;
 	
-	//채팅방 불러오기
-	public List<ChatroomVO> getChatroom(){
-		return mainDAO.getChatroom();
-	}
-	//채팅방 생성
-	public void createChatroom(ChatroomVO chat) {
-		mainDAO.createChatroom(chat);
-	}
-	
-	//채팅방 검색
-	public List<ChatroomVO> searchChatroom(String keyword){
-		return mainDAO.searchChatroom(keyword);
-	}
-	//채팅방 정보
-	public ChatroomVO getRoomInfo(String roomNum) {
-		return mainDAO.getRoomInfo(roomNum);
-	}
-	
-	
+		//채팅방 불러오기
+		public List<ChatroomVO> getChatroom(){
+			return mainDAO.getChatroom();
+		}
+		//빈 방은 삭제
+		/*
+		 * public void emptyRoom() { mainDAO.emptyRoom(); }
+		 */
+		//채팅방 생성
+		public void createChatroom(ChatroomVO chat) {
+			mainDAO.createChatroom(chat);
+		}
+		//채팅방 검색
+		public List<ChatroomVO> searchChatroom(String keyword){
+			return mainDAO.searchChatroom(keyword);
+		}
+		//채팅방 정보
+		public ChatroomVO getRoomInfo(String roomNum) {
+			return mainDAO.getRoomInfo(roomNum);
+		}
+		//채팅방 삭제
+		public void deleteChatroom(String roomNum) {
+			System.out.println(roomNum);
+			mainDAO.deleteChatroom(roomNum);
+		}
 }
