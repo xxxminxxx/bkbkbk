1f15cd8f7bd621bc69ab18f8489842404daa7f93
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-22 14:37:01 +0900

    카테고리 소분류 탭 추가

--- com.bookbookbook.service/LibraryServiceImpl.java
+++ com.bookbookbook.service/LibraryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bookbookbook.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,17 @@ public class LibraryServiceImpl implements LibraryService {
 	private LibraryDAO libraryDAO;
 	
 	//책 카테고리 검색
+	//대분류
 	public List<CategoryVO> getCategory(CategoryVO cate){
 		List<CategoryVO> result = libraryDAO.getCategory(cate);
 		return result;
 	}
-	
+	//중분류
+	public List<CategoryVO> getCategorySecond(CategoryVO cate){
+		List<CategoryVO> result = libraryDAO.getCategorySecond(cate);
+		return result;
+	}
+	//소분류 선택
 	public List<BookVO> getCateBookList(HashMap map){
 		List<BookVO> result= libraryDAO.getCateBookList(map);
 		return result;
