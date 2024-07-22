dbd5605db99667560552e35a5bb3cf4a32a36a01
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-07-22 18:39:12 +0900

    채팅방 삭제

--- com.bookbookbook.service/MainService.java
+++ com.bookbookbook.service/MainService.java
@@ -6,16 +6,16 @@ import com.bookbookbook.domain.ChatroomVO;
 
 public interface MainService {
 	//베스트셀러, 신간, 뉴스 추천, 채팅방
-	
+
 	//채팅방 불러오기
 	public List<ChatroomVO> getChatroom();
+	//public void emptyRoom();
 	//채팅방 생성
 	public void createChatroom(ChatroomVO chat);
 	//채팅방 검색
 	public List<ChatroomVO> searchChatroom(String keyword);
 	//채팅방 정보 불러오기
 	public ChatroomVO getRoomInfo(String roomNum);
-	
-	
-	
+	//채팅방 삭제
+	public void deleteChatroom(String roomNum);
 }
