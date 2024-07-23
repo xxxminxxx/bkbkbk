8ccbf05fd72017d2c4050c066e7effe48c22dc78
minchi <87708449+minchijo@users.noreply.github.com>
2024-07-23 17:27:14 +0900

    책 상세 기능 추가

--- BookService.java
+++ BookService.java
@@ -6,11 +6,12 @@ import com.bookbookbook.domain.BookVO;
 
 public interface BookService {
   
+	//책 제목, 작가, 출판사, 책 소개 별 검색 기능
 	  List<BookVO> bookSearchResultByTitle(String bookTitle);
-	  
 	  List<BookVO> bookSearchResultByWriter(String writer);
-	  
 	  List<BookVO> bookSearchResultByPublisher(String publisher);
-	  
 	  List<BookVO> bookSearchResultByBookPreview(String bookPreview);
+	  
+	  //책 상세 
+	  BookVO getBookDetails(Integer bookNum);
 }
