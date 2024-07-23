570020e9fa628f4bb78c63e30c2805d4a6176c24
zhrldnpftl <108506648+zhrldnpftl@users.noreply.github.com>
2024-07-23 18:16:37 +0900

    Add files via upload
    0723

---
+++ CharactersVO.java
@@ -0,0 +1,15 @@
+package com.bookbookbook.domain;
+
+import lombok.Data;
+
+@Data
+public class CharactersVO {
+
+	private Integer characterNum;
+	private	String	characterName;
+	private String	cfrealname;
+	private String	cfname;
+	private	String	characterBirth;
+	private	Integer	characterHeight;
+	private	String	characterDescription;
+}
