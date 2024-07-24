440377f191bb6c4571aacab7fc46ed0bba7e7747
CYJMK1 <110256640+CYJMK1@users.noreply.github.com>
2024-07-24 11:39:24 +0900

    인기 대출 도서를 도서정보나루 API를 이용하여 시현

--- example/domain/BestsellerVO.java
+++ example/domain/BestsellerVO.java
@@ -1,18 +1,15 @@
-package com.example.domain;
+package com.bookbookbook.domain;
 
 import lombok.Data;
 
 @Data
 public class BestsellerVO {
-
-   private Integer bestNum;
-   private Integer cateNum;
-   private String bestTitle;
-   private Integer bestIsbn;
-   private String bestWriter;
-   private String bestPublisher;
-   private String bestPubDate;
-   private String bestfrealname;
-   private String bestfname;
-   private String bestPrev;
+    private Integer bestNum;
+    private String cateNum;
+    private String bestTitle;
+    private String bestIsbn;
+    private String bestWriter;
+    private String bestPublisher;
+    private String bestPubDate;
+    private String bestfname;
 }
