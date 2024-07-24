2788d28c4640649e5f26c705a7a71cf65da11bee
zhrldnpftl <108506648+zhrldnpftl@users.noreply.github.com>
2024-07-24 14:55:15 +0900

    Update UserDAO.java
    0724

--- UserDAO.java
+++ UserDAO.java
@@ -31,10 +31,8 @@ public interface UserDAO {
     
     //##############################
     // 나의 달력
-    	// 출석 정보 조회
-    public List<CalendarVO> getAttendancesByUserId(String userId);
     	// 메모 정보 조회
-    public List<MemoVO> getMemosByUserId(String userId);
+    public List<HashMap<String, Object>> getMemosAtCalendar(String userId);
     	// 출석체크 데이터베이스에 저장
  	public Integer insertAttendance(String userId);
  		// 데이터베이스에서 출석 정보 조회
