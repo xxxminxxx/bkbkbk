c673a5bfbdb27c0a62540c7122b87cce787ce405
zhrldnpftl <108506648+zhrldnpftl@users.noreply.github.com>
2024-07-30 12:53:30 +0900

    Update UserDAO.java
    0730 UserDAO 나의 캐릭터 수정

--- UserDAO.java
+++ UserDAO.java
@@ -31,15 +31,26 @@ public interface UserDAO {
     
     //##############################
     // 나의 달력
-    	// 출석 정보 조회
-    public List<CalendarVO> getAttendancesByUserId(String userId);
     	// 메모 정보 조회
-    public List<MemoVO> getMemosByUserId(String userId);
+    public List<HashMap<String, Object>> getMemosAtCalendar(String userId);
     	// 출석체크 데이터베이스에 저장
  	public Integer insertAttendance(String userId);
  		// 데이터베이스에서 출석 정보 조회
  	public List<CalendarVO> getAttendanceList(String userId);
-    
+ 	
+ 	//##############################
+ 	// 나의 메모
+ 		// 메모 정보 조회
+ 	public List<HashMap<String, Object>> getMemosAtMemo(String userId);
+ 		// 메모 상세 조회
+ 	public List<HashMap<String, Object>> getMemoDetailByMemoNum(HashMap<String, Object> params);
+ 		// 메모 삭제
+ 	public int memoDelete(HashMap<String, Object> params);
+ 		// 메모 수정
+ 	public int updateMemo(HashMap<String, Object> params);
+ 		// memo 테이블에서 shelfNum을 포함하는 레코드가 있는지 확인
+ 	int countMemoForShelf(HashMap<String, Object> params);
+ 	
     //##############################
   	// 나의 캐릭터
   		// userId로 userLevel에 따른 캐릭터 정보 조회
