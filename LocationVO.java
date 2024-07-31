8b0c3a417407bbf017d222a57fba4ad3e8588556
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-07-31 15:12:05 +0900

    Update LocationVO.java
    20240731

--- LocationVO.java
+++ LocationVO.java
@@ -5,9 +5,9 @@ import lombok.Data;
 
 @Data
 public class LocationVO {
-    private Integer localCode;
-    private String doName;
-    private String city;
-    private float longitude;
-    private float latitude;
+    private Integer localCode;  // 지역 코드 (예: 행정구역 코드)
+    private String doName;      // 도 또는 광역시 이름 (예: 서울특별시, 경기도)
+    private String city;        // 시/군/구 이름 (예: 강남구, 수원시)
+    private float longitude;    // 경도
+    private float latitude;     // 위도
 }
