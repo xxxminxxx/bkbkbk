92d6ea2c17e2d00f11bd48387c1f309b1c20c2d6
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-07-31 13:04:30 +0900

    Update UserStatServiceImpl.java
    20240731

--- UserStatServiceImpl.java
+++ UserStatServiceImpl.java
@@ -21,23 +21,22 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class UserStatServiceImpl implements UserStatService{
 
-
+    // UserStatDAO 객체를 주입받음 (final 키워드와 @RequiredArgsConstructor에 의해 생성자 주입이 이루어짐)
     private final UserStatDAO userStatDAO;
 
-    //권수별 통계
+    // 페이지별 통계를 가져오는 메서드
     @Override
     public List<UserStatVO> getUserStatInfo1(UserStatVO userStatVO) {
+        // UserStatDAO를 통해 권수별 통계 정보를 조회
         List<UserStatVO> userStatVO1 = userStatDAO.getUserStatInfo1(userStatVO);
         return userStatVO1;
-
     }
-    //페이지별 통계
+
+    // 권수별 통계를 가져오는 메서드
     @Override
     public List<UserStatVO> getUserStatInfo2(UserStatVO userStatVO) {
         List<UserStatVO> userStatVO2 = userStatDAO.getUserStatInfo2(userStatVO);
-        System.out.println(userStatVO2);
         return userStatVO2;
-
     }
 
 }
