a069d0bb8a4eccaf4d301e0b88b8e6d07150e2bb
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-07-31 15:09:26 +0900

    Update LocationServiceImpl.java
    20240731

--- LocationServiceImpl.java
+++ LocationServiceImpl.java
@@ -10,10 +10,11 @@ import com.bookbookbook.dao.LocationDAO;
 @Service
 public class LocationServiceImpl implements LocationService {
 
+    // LocationDAO 의존성 주입
     @Autowired
     private LocationDAO locationDAO;
 
-
+    /*특정 도 또는 광역시에 속한 시/군/구 목록을 조회하는 메서드*/
     public List<LocationVO> getCitiesByDoName(String doName) {
         System.out.println("getCitiesByDoName 실행 doname" + doName);
 
