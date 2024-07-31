b2b7f7c4c28af240fc93e18dacc782fc4d7485b8
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-07-31 12:33:40 +0900

    Update UserStatController.java
    20240731

--- UserStatController.java
+++ UserStatController.java
@@ -14,44 +14,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+//필수 필드에 대한 생성자를 자동으로 생성 (Lombok 어노테이션)
 @RequiredArgsConstructor
 public class UserStatController {
-	// 로거 설정
+
+	// 로깅을 위한 Logger 객체를 생성
 	private static final Logger log = LoggerFactory.getLogger(UserStatController.class);
 
-
+	// 생성자 주입을 통해 서비스 객체들을 주입받기
 	private final UserService userService;
 	private final UserStatService userStatService;
 
-	// 사용자 등록 요청을 처리하는 메서드
+
+	//GET 요청으로 나의 통계('/pages/user-myStat') 경로에 접근했을 때 처리하는 메서드입
 	@GetMapping("/pages/user-myStat")
 	public String userStat(@ModelAttribute UserStatVO userStatVO, HttpSession session, Model model) {
 
-		//로그인한 userId
+		//로그인한 userId (세션에서 로그인한 사용자의 ID를 가져와 UserStatVO 객체에 설정)
 		userStatVO.setUserId((String) session.getAttribute("userId"));
-		if(userStatVO.getUserId() == null){
-			log.info("로그인이 안되어 있어서 로그인으로 이동");
-			return "redirect:/pages/login";
-		}
-		//bookshelf 쓸 userNum 뽑아오기
+
+		//나의 서재(bookshelf)에서 쓸 userNum 뽑아오기 (사용자 ID를 이용해 사용자 정보를 조회하고, 사용자 번호를 UserStatVO 객체에 설정)
 		UserVO userVO = userService.getUserById(String.valueOf(userStatVO.getUserId()));
 		userStatVO.setUserNum(userVO.getUserNum());
-		//끝
 
-
-		//권수별 통계
+		//권수별 통계 정보를 조회
 		List<UserStatVO> userStatVO1= userStatService.getUserStatInfo1(userStatVO);
-		//페이지별 통계
+		//페이지별 통계 정보를 조회
 		List<UserStatVO> userStatVO2= userStatService.getUserStatInfo2(userStatVO);
 
-		
+
+		// 조회한 통계 정보를 모델에 추가
 		model.addAttribute("userStatVO", userStatVO1);
 		model.addAttribute("userStatVO2", userStatVO2);
+		// 로그 메시지를 출력
 		log.info("로그인 되있어서 나의 통계로 이동");
+		//('pages/user-myStat')뷰를 반환
 	    return "pages/user-myStat";
 
-
 	}
-
 
 }
