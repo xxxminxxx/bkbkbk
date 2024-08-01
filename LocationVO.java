1f5d1c135e2cea442a7cd9603594b8a87ca8a7ab
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-08-01 17:46:58 +0900

    Create LocationVO.java
    20240801

---
+++ LocationVO.java
@@ -0,0 +1,13 @@
+package com.bookbookbook.domain;
+
+import lombok.Data;
+
+
+@Data
+public class LocationVO {
+	private Integer localCode;           // 지역 코드
+    private String doName;               // 도/광역시 이름
+    private String city;                 // 시/군/구 이름
+    private float longitude;             // 경도
+    private float latitude;              // 위도
+}
