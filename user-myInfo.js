72061851f5def2d6a850cec9d6a45783dcdb553d
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-08-02 17:47:10 +0900

    Update user-myInfo.js
    20240802

--- user-myInfo.js
+++ user-myInfo.js
@@ -6,8 +6,27 @@ $('#frmMyInfo')
 
         const password = $("input[name=password]").val();
         const password2 = $("input[name=password2]").val();
+        const userName = $("input[name=userName]").val();
+        const userTel = $("input[name=userTel]").val();
 
-        // 비밀번호 필드에 입력이 있을 때만 유효성 검사
+        // 이름 유효성 검사
+        if (userName.trim() === '') {
+            alert('이름을 입력해주세요.');
+            return false;
+        }
+        if (userName.length > 50) {
+            alert('이름은 50자 이내로 입력해주세요.');
+            return false;
+        }
+
+        // 전화번호 유효성 검사
+        const telRegex = /^01[016789]-?[^0][0-9]{2,3}-?[0-9]{3,4}$/;
+        if (!telRegex.test(userTel)) {
+            alert('올바른 전화번호 형식이 아닙니다.');
+            return false;
+        }
+
+        // 비밀번호 필드에 입력이 있을 때만 유효성 검사를 수행합니다.
         if (password || password2) {
             if (password !== password2) {
                 alert('비밀번호가 일치하지 않습니다.');
@@ -20,12 +39,12 @@ $('#frmMyInfo')
             }
 
             if (!/[A-Z]/.test(password)) {
-                alert('대문자를 포함해야 합니다.');
+                alert('비밀번호에 대문자를 포함해야 합니다.');
                 return false;
             }
 
             if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
-                alert('특수문자를 포함해야 합니다.');
+                alert('비밀번호에 특수문자를 포함해야 합니다.');
                 return false;
             }
         }
