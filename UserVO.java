f95588556376de33a639a14c0e2bcd72f2caaeb0
euncoder-com <167957759+euncoder-com@users.noreply.github.com>
2024-08-02 11:49:12 +0900

    Update UserVO.java
    20240802

--- UserVO.java
+++ UserVO.java
@@ -5,21 +5,23 @@ import lombok.Data;
 @Data
 public class UserVO {
 	
-	private Integer userNum;
-	private String userId;
-	private String password;
-	private String password2; /*추가*/
-	private String userName;
-	private String userTel;
-	private String joinedDate;
-	private String birthDate;
-	private String nickname;
-	private String gender;
-	private Integer quitStatus;
-	private Integer banStatus;
-	private Integer userLevel;
-	private String kakaoId;
-	private String naverId;
-	private String reported;
+	private Integer userNum;     // 사용자 고유 번호
+	private String userId;       // 사용자 아이디
+	private String password;     // 사용자 비밀번호
+	private String password2;    // 비밀번호 확인용
+	private String userName;     // 사용자 이름
+	private String userTel;      // 사용자 전화번호
+	private String joinedDate;   // 가입 날짜
+	private String birthDate;    // 생년월일
+	private String nickname;     // 사용자 닉네임
+	private String gender;       // 성별
+	private String quitStatus;   // 탈퇴 상태
+	private String banStatus;    // 정지 상태
+	private Integer userLevel;   // 사용자 레벨
+	private String kakaoId;      // 카카오 계정 연동 ID
+	private String naverId;      // 네이버 계정 연동 ID
+	private String reported;     // 신고 여부
+	private String interestHobby; // 관심사/취미
+	private String address;       // 주소
 	
 }
