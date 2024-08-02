8aed804858b06e208d1645c4b15e8c2c6e9272e1
GenieJ28 <167986889+GenieJ28@users.noreply.github.com>
2024-08-02 11:17:22 +0900

    기능 업데이트

--- com.bookbookbook.dao/MainDAO.java
+++ com.bookbookbook.dao/MainDAO.java
@@ -3,23 +3,35 @@ package com.bookbookbook.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import com.bookbookbook.domain.BookVO;
+import com.bookbookbook.domain.BookshelfVO;
+import com.bookbookbook.domain.CategoryVO;
 import com.bookbookbook.domain.ChatroomVO;
 
 @Mapper
 public interface MainDAO {
 
-	//##################################
-	//채팅방 불러오기
-	public List<ChatroomVO> getChatroom();
-	//빈 방 삭제
-	//public void emptyRoom();
-	//방 생성
-	public void createChatroom(ChatroomVO chat);
-	//방 검색
-	public List<ChatroomVO> searchChatroom(String keyword);
-	//방 정보 가져오기
-	public ChatroomVO getRoomInfo(String roomNum);
-	//방 삭제
-	public void deleteChatroom(String roomNum);
+	 //##################################
+	   //채팅방 불러오기
+	   public List<ChatroomVO> getChatroom();
+	   //유저 책장 불러오기
+	   public List<BookshelfVO> getMyBookshelf(String userNum);
+	   //방 생성
+	   public void createChatroom(ChatroomVO chat);
+	   //유저 번호 불러오기
+	   public String getUserID(String userId);
+	   //방 검색
+	   public List<ChatroomVO> searchChatroom(String keyword);
+	   //방 정보 가져오기
+	   public ChatroomVO getRoomInfo(String roomNum);
+	   //방 삭제
+	   public void deleteChatroom(String roomNum);
+
+	
+	//######################################
+  	//서울시 희망 여가 활동 데이터 처리
+	  List<Integer> findCateNumByPreference(@Param("preference") String preference);
+  List<BookVO> findBookByCateNum(@Param("cateNum") Integer cateNum);
 }
